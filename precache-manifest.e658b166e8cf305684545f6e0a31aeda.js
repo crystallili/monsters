ced3b3177761a3372cbbbdb94bd9ef15a782fbb0
Lili Tong <281513504@qq.com>
2020-05-09 23:50:48 -0700

    Updates

---
+++ precache-manifest.e658b166e8cf305684545f6e0a31aeda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d930e12797723ff2e8d721e4efe14c7",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "63059b3ae2f2ad112387",
+    "url": "/monsters/static/css/main.f74ba966.chunk.css"
+  },
+  {
+    "revision": "aa86dba7ccdda618ee4d",
+    "url": "/monsters/static/js/2.f5912c83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.f5912c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63059b3ae2f2ad112387",
+    "url": "/monsters/static/js/main.7abc41a2.chunk.js"
+  },
+  {
+    "revision": "816fb60049de7838f9b3",
+    "url": "/monsters/static/js/runtime-main.58e04e72.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
